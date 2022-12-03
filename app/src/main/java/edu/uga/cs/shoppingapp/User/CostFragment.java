f660d42a4037930a26c93574eefecdabf7e64ce0
Tommy <tjfricano@gmail.com>
2022-12-02 21:08:45 -0500

    user purchases loaded into cost fragment

--- app/src/main/java/edu/uga/cs/shoppingapp/User/CostFragment.java
+++ app/src/main/java/edu/uga/cs/shoppingapp/User/CostFragment.java
@@ -7,15 +7,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.uga.cs.shoppingapp.Adapters.CartRecyclerAdapter;
@@ -29,38 +34,19 @@ import edu.uga.cs.shoppingapp.R;
  */
 public class CostFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String DEBUG_TAG = "CostFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private RecyclerView recyclerView;
-    private CartRecyclerAdapter recyclerAdapter;
-    private List<String> userList;
+    private List<User> userList;
     private FirebaseDatabase database;
 
     public CostFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CostFragment newInstance(String param1, String param2) {
+    public static CostFragment newInstance() {
         CostFragment fragment = new CostFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
 
         return fragment;
@@ -70,8 +56,6 @@ public class CostFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
     }
@@ -86,21 +70,40 @@ public class CostFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
         super.onViewCreated(view, savedInstanceState);
 
-//        recyclerView = getView().findViewById( R.id.recyclerView1);
-//
-//        // initialize the items list
-//        userList = new ArrayList<String>();
-//
-//        // use a linear layout manager for the recycler view
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-//        recyclerView.setLayoutManager(layoutManager);
-//
-//        // the recycler adapter with items is empty at first; it will be updated later
-//        recyclerAdapter = new CartRecyclerAdapter( userList, getActivity(), getChildFragmentManager());
-//        recyclerView.setAdapter( recyclerAdapter );
-//
-//        // get a Firebase DB instance reference
-       database = FirebaseDatabase.getInstance();
+        userList = new ArrayList<User>();
+
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("purchased");
+        ref.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
+                userList.clear(); // clear the current content; this is inefficient!
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    Log.d( DEBUG_TAG, "ValueEventListener: " + postSnapshot.getValue(User.class));
+                    User user = postSnapshot.getValue(User.class);
+                    user.setKey( postSnapshot.getKey() );
+                    userList.add( user );
+                }
+            }
+
+            @Override
+            public void onCancelled( @NonNull DatabaseError databaseError ) {
+                System.out.println( "ValueEventListener: reading failed: " + databaseError.getMessage() );
+            }
+        } );
+
+        for(int i=0;i<userList.size();i++){
+            Log.d( DEBUG_TAG, "item:" + userList.get(i).getEmail() +"spent: "+userList.get(i).getSpent());
+        }
+
+
+//        Todo: all values are in the userlist
+
+
+
+//      get a Firebase DB instance reference
+//       database = FirebaseDatabase.getInstance();
        DatabaseReference myRef = database.getReference("cart");
        Button button = getView().findViewById(R.id.button2);
        button.setOnClickListener( new View.OnClickListener() {
